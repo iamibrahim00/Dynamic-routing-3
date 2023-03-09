3c98fd42371d22909dab3b17533c1807b57f5aca
iamibrahim00 <iyamibrahimshaikh@gmail.com>
2023-03-09 14:52:31 +0530

    Dynamic Routin code 2 adding the cart value and details

---
+++ controllers/shop.js
@@ -0,0 +1,70 @@
+const Product = require('../models/product');
+const Cart = require('../models/cart');
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products'
+    });
+  });
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
+  });
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    path: '/cart',
+    pageTitle: 'Your Cart'
+  });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
+};
+
+exports.getContact = (req, res, next) => {
+  res.render('contact', {
+    path: '/contact',
+    pageTitle: 'Contact Details'
+  });
+};
+
+exports.postContact = (req, res, next) => {
+  res.redirect('/success') 
+ 
+};
+exports.getsuccess = (req, res, next) => {
+  res.render('success', {
+    pageTitle: 'success',
+    path: '/success',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
+ 
+};
---
+++ routes/contact.js
@@ -0,0 +1,11 @@
+const path = require('path')
+const express= require('express');
+const router = express.Router()
+const rootDir = require('../helper/path')
+const getContact =require('../controllers/contact')
+const postControler =require('../controllers/contact')
+
+router.get('/contact',getContact.getContactController);
+
+router.post('/contact',postControler.postContactController)
+module.exports = router 
